13d0d810f5a444a451a1bf06060852ed92575e80
jankiel7410 <michal7410@gmail.com>
2014-03-09 22:30:37 +0100

    więzanie wszystkiego razem - podejście 1

--- tsp.py
+++ tsp.py
@@ -4,10 +4,12 @@ import math
 
 #dystans pomiedzy dwoma miastami
 
+
 def get_distance(p1, p2):
     dx = p1[0] - p2[0]
     dy = p1[1] - p2[1]
     return math.sqrt(dx**2 + dy**2)
+
 
 def rate_solution(points):
     dist_sum = 0
