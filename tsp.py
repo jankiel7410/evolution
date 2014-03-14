44e4dde45413ab83b133c0941df8919f23193aab
jankiel <michal7410@gmail.com>
2014-03-14 12:26:22 +0100

    nowa ruletka, to już chyba ostateczna wersja
    zmiany w Individual:
    - krzyżowanie zawsze się wykonuje
    - dodana metoda walidująca
    
    zmiany w Population:
    - właściwie przerobione wszystkie metody
    - konstruktor bierze wszystkie 4 kluczowe parametry
    
    dodana klasa InfoGrabber, ale prawdopodobnie do przepisania
    
    Jakieś zmiany w TSP

--- tsp.py
+++ tsp.py
@@ -5,15 +5,21 @@ import math
 #dystans pomiedzy dwoma miastami
 
 
-class Troll:
-
-    def __init__(self, points):
-        self.points = points
-
-    def get_distance(self, p1, p2):
+def get_dist(p1, p2):
         dx = p1[0] - p2[0]
         dy = p1[1] - p2[1]
         return math.sqrt(dx**2 + dy**2)
 
+
+class Troll:
+    def __init__(self, points):
+        self.first = points[0]#pierwsze miasto zawsze jest pierwszym miastem
+        self.points = points[1:]
+
     def rate_solution(self):
-        pass
+        total_dist = 0
+        total_dist += get_dist(self.first, self.points[-1])#dystans pomiędzy pierwszym a ostatnim
+        curr = self.first
+        for p in self.points:
+            total_dist += get_dist(curr, p)
+            curr = p
